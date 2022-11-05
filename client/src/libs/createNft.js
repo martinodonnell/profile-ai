6c27ea842009c8525c4b1794574c999d5b0437a9
Jack Collins <jackmpcollins@gmail.com>
2022-11-05 16:23:45 -0700

    Return ipfs url

--- client/src/libs/createNft.js
+++ client/src/libs/createNft.js
@@ -23,11 +23,11 @@ async function createNftFromUrl(name, description, url) {
   //   });
   const cidString = await client.storeBlob(data);
   //   console.log(metadata);
-  return cidString;
+  return `ipfs://${cidString}`;
 }
 
 // let imageUrl =
 //   "https://oaidalleapiprodscus.blob.core.windows.net/private/org-HOSkiZGfyJeiuj3jOvsiJ2Lg/user-Kvuaup7JzD9jWvgR4FX732fB/img-1Cm18iwJwhgoEb2LLtsaAcDO.png?st=2022-11-05T22%3A16%3A03Z&se=2022-11-06T00%3A16%3A03Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-11-05T02%3A25%3A05Z&ske=2022-11-06T02%3A25%3A05Z&sks=b&skv=2021-08-06&sig=AUY%2BubXWCHn7WVjnuWKeLio9AtF4hCMPq6ya5rJguxA%3D";
-// // let imageUrl = "https://static.thenounproject.com/png/4778723-200.png";
+// let imageUrl = "https://static.thenounproject.com/png/4778723-200.png";
 
 // createNftFromUrl("test", "This is a test 1", imageUrl).then(console.log);
